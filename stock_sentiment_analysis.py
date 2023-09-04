c87d99d32592673d28068d1f951d67f9b443d9b2
Olaseeni Olayinka <46079073+OlaseeniOlayinka@users.noreply.github.com>
2023-09-04 15:21:55 -0700

    can now get price change in a quarter

--- stock_sentiment_analysis.py
+++ stock_sentiment_analysis.py
@@ -4,6 +4,9 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import tkinter as tk
 from textblob import TextBlob
+import yfinance as yf
+from datetime import datetime
+import json, sys
 
 #url = "https://www.fool.com/earnings/call-transcripts/2023/02/02/apple-aapl-q1-2023-earnings-call-transcript/"
 
@@ -13,12 +16,7 @@ def fetch_transcript():
     transcript_html = response.text
     soup = BeautifulSoup(transcript_html, "html.parser")
 
-    prepared_remarks = ""
-    q_and_a = ""
-    call_participants = ""
-
     included_class = "tailwind-article-body"
-    #excluded_class = ["article-pitch-container", "image imgR"]
     excluded_class = "article-pitch-container"
 
     transcript_element = soup.find_all("div", class_= included_class)
@@ -34,11 +32,11 @@ def fetch_transcript():
 
     for element in transcript_element:
         transcript_text += element.get_text() + "\n"
-    
+
     # perform sentiment analysis
     blob = TextBlob(transcript_text)
     sentiment_score = blob.sentiment.polarity
-    print(sentiment_score)
+    print("Sentiment analysis score: ", sentiment_score)
         
     # Create a WordCloud object
     wordcloud = WordCloud(width=800, height=400, background_color='white').generate(transcript_text)
@@ -48,6 +46,43 @@ def fetch_transcript():
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.axis('off')
     plt.show()
+
+def stock_price_change():
+    quarter = quarter_entry.get().upper()
+    start_date = ""
+    end_date = ""
+
+    if quarter == "Q1":
+        start_date = "2023-01-01"
+        end_date = "2023-03-31"
+
+    elif quarter == "Q2":
+        start_date = "2023-04-01"
+        end_date = "2023-06-30"
+    
+    elif quarter == "Q3":
+        start_date = "2023-07-01"
+        end_date = "2023-09-30"
+    
+    elif quarter == "Q4":
+        start_date = "2023-10-01"
+        end_date = "2023-12-31"
+    
+    else:
+        return "Invalid quarter entered"
+
+    ticker_from_user = ticker_entry.get().upper()
+    ticker = yf.Ticker(ticker_from_user)
+    ticker_history = ticker.history(start=start_date, end=end_date)
+
+    print(ticker_history)
+    
+    quarter_start_stock_price = ticker_history.loc[:, "Open"][0]
+    quarter_end_stock_price = ticker_history.loc[:, "Close"][-1]
+    quarter_return = (quarter_end_stock_price - quarter_start_stock_price) * 100 / quarter_start_stock_price
+    print("Return during requested quarter for %s : %d %%" % (ticker_from_user, quarter_return))
+
+    return -1
 
 def quit_app():
     root.destroy()
@@ -64,6 +99,26 @@ url_entry = tk.Entry(root, width=50)
 url_entry.pack()
 
 fetch_button = tk.Button(root, text="Fetch Transcript", command=fetch_transcript)
+fetch_button.pack()
+
+# for the quarter
+quarter_label = tk.Label(root, text = "\n\nEnter The Relevant Quarter e.g. Q1 or q1")
+quarter_label.pack()
+
+quarter_entry = tk.Entry(root, width=50)
+quarter_entry.pack()
+
+fetch_button = tk.Button(root, text="Fetch stock change in quarter")
+fetch_button.pack()
+
+# for the ticker
+ticker_label = tk.Label(root, text = "\n\nEnter The Ticker")
+ticker_label.pack()
+
+ticker_entry = tk.Entry(root, width=50)
+ticker_entry.pack()
+
+fetch_button = tk.Button(root, text="Fetch ticker results in quarter", command=stock_price_change)
 fetch_button.pack()
 
 transcript_textbox = tk.Text(root, height=20, width=80)
